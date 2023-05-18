5742a2a481dcc58ea4ccdb815a0689260f1dd9d0
David Sánchez Vargas <david@192.168.0.7>
2023-05-17 20:05:23 -0500

    Back terminado

--- api/src/controllers/getDogsByName.js
+++ api/src/controllers/getDogsByName.js
@@ -1,19 +1,36 @@
-const { Dog } = require("../db");
+const { Dog, Temperament, Op } = require("../db");
+const axios = require("axios");
 
 const getDogsByName = async (req, res) => {
   const { name } = req.query;
+  const responseNameApi = await axios.get(
+    "https://api.thedogapi.com/v1/breeds"
+  );
+
   if (name === "") {
     return res
       .status(400)
       .json({ message: "Debe digital el nombre del perro que desea obtener" });
   }
   try {
-    let response = await Dog.findOne({ where: { name } });
-    //({where: { name: {[Op.iLike]: "%" + name + "%",},}, include: {model: Temper}});
-    if (!response) {
+    let findDogDb = await Dog.findAll({
+      where: {
+        name: { [Op.iLike]: `%${name}%` },
+      },
+      include: { model: Temperament },
+    });
+    //({where: { name: {[Op.iLike]: %" + name + "%",},}, include: {model: Temper}});
+    let findDogApi = responseNameApi.data.filter(
+      (dog) => dog.name.toLowerCase() === name.toLowerCase()
+    );
+
+    let dogFound = [...findDogApi, ...findDogDb];
+
+    if (!dogFound) {
       return res.status(400).json({ message: "Perro no encontrado" });
     }
-    return res.status(200).json(response);
+
+    return res.status(200).json(dogFound);
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
